64c08f64cd4fcdc810fa21946aac68dbe2fa4f8b
Juan <juanmetchart@gmail.com>
2025-09-29 17:47:55 -0300

    Production configuration

--- src/config/config.production.ts
+++ src/config/config.production.ts
@@ -3,19 +3,19 @@ import { DEFAULT_DATA_SOURCE_OPTIONS } from './config.common';
 export const configuration = () => ({
   database: {
     ...DEFAULT_DATA_SOURCE_OPTIONS,
-    url: process.env.DB_URL || 'postgresql://postgres:bmxUtMuOvMdPDmQCrJkvlmWoSrbgLkVf@postgres.railway.internal:5432/railway',
-    synchronize: true,
+    url: process.env.DB_URL,
+    synchronize: false,
     autoLoadEntities: true,
     logging: false,
   },
   jwt: {
-    secret: process.env.JWT_SECRET || 'THE_SECRET',
+    secret: process.env.JWT_SECRET,
   },
   google: {
-    clientId: "65738319816-84n4osqr38kdjhqdrr7q0a0id2d56gre.apps.googleusercontent.com"
+    clientId: process.env.GOOGLE_CLIENT_ID
   },
   web: {
-    host: 'http://localhost:5173',
+    host: process.env.WEB_HOST,
   },
-  nestport: 3000,
+  nestport: process.env.NEST_PORT,
 });
